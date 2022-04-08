759844043562193a6777e80dce6bffb4626157bf
hoenn <hoenn@github.com>
2022-04-08 10:32:31 -0400

    init commit

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/hoenn/whohirin
+
+go 1.15
+
+require github.com/hoenn/go-hn v0.0.0-20210922140512-4341d078d03b
---
+++ main.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/hoenn/go-hn/pkg/hnapi"
+)
+
+var client *hnapi.HNClient
+
+func main() {
+	fmt.Println()
+}
